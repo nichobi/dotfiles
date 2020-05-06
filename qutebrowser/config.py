2bb67659f20dc91c99bd8f34b0c749a11998fdfc
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2020-05-06 19:54:00 +0200

    qutebrowser: Add qute-pass key-bindings

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -1,2 +1,7 @@
 config.load_autoconfig()
 c.editor.command = ["alacritty", "-t", "Qute Editor", "-d", "80", "24", "-e", "nvim", "{}", "+call cursor({line}, {column})"]
+config.bind('<z><l>', 'spawn --userscript qute-pass')
+config.bind('<z><u><l>', 'spawn --userscript qute-pass --username-only')
+config.bind('<z><p><l>', 'spawn --userscript qute-pass --password-only')
+config.bind('<z><o><l>', 'spawn --userscript qute-pass --otp-only')
+
