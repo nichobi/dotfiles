20a2221121d42bb4a819742a6cb55e5acce9c966
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2020-05-20 12:16:11 +0200

    waybar: Add keyboard layout indicator

---
+++ waybar/modules/kblayout.sh
@@ -0,0 +1,9 @@
+swaymsg -t get_inputs | jq -r \
+    "first(.[]|select(.identifier == \"$1\" and .type == \"keyboard\")) \
+    | .xkb_active_layout_name | .[0:2] | ascii_downcase"
+
+swaymsg -mrt subscribe '["input"]' | jq -r --unbuffered \
+    "select(.change == \"xkb_layout\")
+    | .input
+    | select(.identifier == \"$1\" and .type == \"keyboard\") \
+    | .xkb_active_layout_name | .[0:2] | ascii_downcase"
