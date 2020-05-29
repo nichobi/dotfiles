d4832d3174fcd781863cae574919d29d99f58646
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2020-05-30 00:54:24 +0200

    sway: Retheme swaylock screen

--- sway/lock.sh
+++ sway/lock.sh
@@ -6,12 +6,24 @@ swaylock -f -K --clock -c 000000 \
 	 --indicator-radius 100 \
 	 --indicator-thickness 7 \
 	 --ignore-empty-password \
+   \
 	 --ring-color 303030 \
 	 --key-hl-color 808080 \
 	 --text-color ffffff \
 	 --line-color 00000000 \
 	 --inside-color 00000088 \
 	 --separator-color 00000000 \
+   \
+	 --ring-ver-color ffffff \
+	 --text-ver-color ffffff \
+	 --line-ver-color 00000000 \
+	 --inside-ver-color 00000088 \
+   \
+	 --ring-wrong-color ff361e \
+	 --text-wrong-color ff361e \
+	 --line-wrong-color 00000000 \
+	 --inside-wrong-color 00000088 \
+   \
 	 --fade-in 0.5 \
 	 "$@"
 	 #--effect-scale 0.5 --effect-blur 7x3 --effect-scale 2 \
