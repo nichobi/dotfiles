e5056f6c3d556580a591f397e357b194789a6f5d
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2020-06-16 12:20:09 +0200

    qutebrowser: Add search engine Dwarf Fortress Wiki

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -55,6 +55,7 @@ c.fonts.default_size = '12pt'
 c.url.searchengines = {
   "DEFAULT": "https://www.google.com/search?q={}",
   "ddg":     "https://duckduckgo.com?q={}",
-  "aw":      "https://wiki.archlinux.org/?search={}"
+  "aw":      "https://wiki.archlinux.org/?search={}",
+  "dfw":      "https://dwarffortresswiki.org?search={}"
 }
 
