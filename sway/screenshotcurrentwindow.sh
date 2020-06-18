0b1a2e9121b04883b975236eeb6c7e114ccf5ed5
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2020-06-18 16:24:38 +0200

    sway: Add script to screenshot current window

---
+++ sway/screenshotcurrentwindow.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+APPNAME=$(swaymsg -t get_tree   | jq -r '.. | (.nodes? // empty)[] | select(.focused==true) | (.app_id // .name)')
+PICDIR=$(xdg-user-dir PICTURES)
+DATE=$(date +'%Y-%m-%d %H-%M-%S')
+
+grimshot save win "$PICDIR/screenshots/$APPNAME $DATE.png"
+
