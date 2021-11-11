f11448857a460ef52d05b0b63e596c2bb0265ca9
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2021-11-11 18:25:02 +0100

    qutebrowser: Redirect reddit.com to reddit.com/hot

---
+++ qutebrowser/greasemonkey/redditredirecthot.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name RedditRedirectHot
+// @namespace nichobi.com
+// @description Redirect Reddit frontpage to hot sort
+// @match https://www.reddit.com/
+// @run-at document-start
+// ==/UserScript==
+window.location.replace("https://www.reddit.com/hot")
+
