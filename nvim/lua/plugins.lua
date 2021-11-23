1a71cc86a4d77bf768ee787453a0d864875149e2
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2021-11-23 16:17:10 +0100

    nvim: Add additional nvim-cmp sources

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -22,10 +22,13 @@ require('packer').startup(function()
   use {
     'hrsh7th/nvim-cmp',
     requires = {
-      'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-calc',
+      'hrsh7th/cmp-cmdline',
+      'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-vsnip',
+      'hrsh7th/vim-vsnip',
     }
   }
 
