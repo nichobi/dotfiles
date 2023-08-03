f5210d63fdbda756f8f614ba6f150ba4360106c5
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2023-08-03 16:58:52 +0200

    qutebrowser: Add dmenu and username-target to pass

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -72,10 +72,10 @@ config.bind('gh', 'set-cmd-text -s :back ')
 config.bind('gl', 'set-cmd-text -s :forward ')
 
 # Binds for qute-pass
-config.bind('zl',  'spawn --userscript qute-pass')
-config.bind('zol', 'spawn --userscript qute-pass --otp-only')
-config.bind('zpl', 'spawn --userscript qute-pass --password-only')
-config.bind('zul', 'spawn --userscript qute-pass --username-only')
+config.bind('zl',  'spawn --userscript qute-pass --username-target secret --username-pattern "username: (.+)" --dmenu-invocation "runin-dmenu"')
+config.bind('zol', 'spawn --userscript qute-pass --otp-only --username-target secret --username-pattern "username: (.+)" --dmenu-invocation "runin-dmenu"')
+config.bind('zpl', 'spawn --userscript qute-pass --password-only --username-target secret --username-pattern "username: (.+)" --dmenu-invocation "runin-dmenu"')
+config.bind('zul', 'spawn --userscript qute-pass --username-only --username-target secret --username-pattern "username: (.+)" --dmenu-invocation "runin-dmenu"')
 
 # Binds for images
 config.unbind(';i')
