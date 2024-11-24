51c74f97625fd641b8b0c39d6f9ea4dc8867c291
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2024-11-24 01:21:32 +0100

    nvim: Match jrag files as java

--- nvim/lua/filetypes.lua
+++ nvim/lua/filetypes.lua
@@ -1,5 +1,12 @@
 local ft = vim.filetype
 
+-- Match jrag files as Java, for roughly correct highlighting
+ft.add({
+  pattern = {
+    [".*.jrag"] = "java",
+  },
+})
+
 -- Match helm template files for treesitter highlighting
 ft.add({
   pattern = {
