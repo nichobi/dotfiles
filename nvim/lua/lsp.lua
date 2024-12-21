06674ae101b815fa35925e94dcd546e59a4f1f61
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2024-12-21 23:26:39 +0100

    nvim: Swap nvim-cmp to blink.cmp

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -7,7 +7,7 @@ cmd('au BufRead,BufNewFile *.sbt set filetype=scala')
 -- Use proper scaladoc comment indentation
 g.scala_scaladoc_indent = 1
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 -- Haskell language server
 require'lspconfig'.hls.setup{capabilities = capabilities}
@@ -72,39 +72,5 @@ cmd [[augroup lsp
 
 opt.completeopt = 'menu,menuone,noselect'
 
-local cmp = require('cmp')
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert(),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'vsnip' },
-  }, {
-    { name = 'buffer' },
-    { name = 'calc' },
-    { name = 'path' },
-  })
-})
-
 
 -- Use buffer source for `/`.
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':'.
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -42,16 +42,11 @@ require('lazy').setup({
 
     -- Completion engine, with snippet plugin
     {
-      'hrsh7th/nvim-cmp',
-      dependencies = {
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-calc',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-vsnip',
-        'hrsh7th/vim-vsnip',
-      }
+      'saghen/blink.cmp',
+      -- optional: provides snippets for the snippet source
+      dependencies = 'rafamadriz/friendly-snippets',
+      version = 'v0.*',
+      opts = {}
     },
 
     -- Basic Scala integration
