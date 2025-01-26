ec0f5d9a701d372e72cedb875544215befa460b1
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2025-01-26 20:43:52 +0100

    zsh: Various cleanup

--- zsh/zshrc.d/alias.zsh
+++ zsh/zshrc.d/alias.zsh
@@ -1,3 +1,4 @@
+alias adb='HOME="$XDG_DATA_HOME/android" adb'
 alias cacavid='mplayer -vo caca -msglevel all=-1 -fixed-vo'
 alias clearall="clear && printf '\e[3J'"
 alias fbm='node /home/nichobi/build/fb-messenger-cli/cli.js'
@@ -14,13 +15,14 @@ alias pR="paru  -Qq  | fzf -m --preview 'cat <(paru -Si {1}) <(paru -Fl {1} | aw
 alias pRe="paru -Qqe | fzf -m --preview 'cat <(paru -Si {1}) <(paru -Fl {1} | awk \"{print \$2}\")' | xargs -ro paru -Rsn"
 alias pS="paru  -Slq | fzf -m --preview 'cat <(paru -Si {1}) <(paru -Fl {1} | awk \"{print \$2}\")' | xargs -ro paru -S"
 alias rsync="rsync -hv --info=progress2"
+alias sbt="sbt -ivy $XDG_DATA_HOME/ivy2 -sbt-dir $XDG_DATA_HOME/sbt"
 alias sudo='sudo ' # Enables alias expansion on commands run with sudo
 alias tb="nc termbin.com 9999"
 alias tmux='tmux -u'
 alias updatemusicfolder="ssh mir 'beet alt update lossy' && rsync -thr --no-i-r --progress mir:/mnt/data/MusicLossy/ ~/Music/ --delete"
 alias webcamdisable='sudo modprobe -r uvcvideo'
 alias webcamenable='sudo modprobe uvcvideo'
-alias wget='wget --content-disposition'
+alias wget='wget --content-disposition --hsts-file="$XDG_DATA_HOME/wget-hsts"'
 alias screenmirrorx11='SDL_VIDEODRIVER=x11 wf-recorder -c rawvideo -m sdl -f pipe:wayland-mirror'
 alias screenmirrorwayland='SDL_VIDEODRIVER=wayland wf-recorder -c rawvideo -m sdl -f pipe:wayland-mirror'
 
