9f59f16a5e763a173f483c2647803af4b0bdf595
Nicholas Boyd Isacsson <nicholas@isacsson.se>
2025-02-16 15:04:27 +0100

    zsh: Don't highlight valid commands green

---
+++ zsh/zshrc.d/visual.zsh
@@ -0,0 +1,7 @@
+typeset -A ZSH_HIGHLIGHT_STYLES
+
+# Don't highlight valid commands in green
+ZSH_HIGHLIGHT_STYLES[command]='none'
+ZSH_HIGHLIGHT_STYLES[alias]='none'
+ZSH_HIGHLIGHT_STYLES[builtin]='none'
+
